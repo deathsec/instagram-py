c6a4cc9097bbd4ded3b22775498f68bde1d78d66
deathsec <antonyjr@protonmail.com>
2017-10-08 14:53:17 +0530

    Changing constants.py to AppInfo.py

---
+++ InstagramPy/AppInfo.py
@@ -0,0 +1,16 @@
+# The MIT License
+# Copyright (C) 2017 The Future Shell , DeathSec
+#
+# @filename    : AppInfo.py
+# @description : Application Information that are required
+
+appInfo = {
+    "version"     : "1.3.2",
+    "name"        : "Instagram-Py",
+    "description" : " Powerful Instagram brute force script",
+    "author"      : "DeathSec",
+    "company"     : "The Future Shell",
+    "year"        : "2017",
+    "example"     : "example: instagram-py -v instatestgod__ rockyou.txt"
+}
+
